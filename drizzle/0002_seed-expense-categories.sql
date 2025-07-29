0250158328a7526212fe9732f8814b27795f1e1a
kyawphyothu <dev@kyawphyothu.com>
2025-07-30 00:13:59 +0700

    Add new database snapshots and update migration scripts
    - Created new snapshots (0002, 0003, 0004) for the database schema, including tables for account categories, accounts, currencies, expense categories, expenses, income categories, and incomes.
    - Updated the journal file to reflect the new migration tags and timestamps.
    - Modified the migrations.js file to import the new SQL migration files for seeding income categories, expense categories, account categories, and currencies.

---
+++ drizzle/0002_seed-expense-categories.sql
@@ -0,0 +1,4 @@
+-- Custom SQL migration file, put your code below! --
+INSERT INTO 'expense_categories' ("name") VALUES ("Rent");--> statement-breakpoint
+INSERT INTO 'expense_categories' ("name") VALUES ("Food");--> statement-breakpoint
+INSERT INTO 'expense_categories' ("name") VALUES ("Transport");
